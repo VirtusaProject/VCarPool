dc63a88d137adfcee498fc724d23a8b1bbfb5188
krishnateja <52774199+VirtusaProject@users.noreply.github.com>
2019-07-12 10:50:15 +0530

    commit

--- databaseVCarPool.sql
+++ databaseVCarPool.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `bill`
 --
-
+create database carpooldb;
+use carpooldb;
 DROP TABLE IF EXISTS `bill`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
